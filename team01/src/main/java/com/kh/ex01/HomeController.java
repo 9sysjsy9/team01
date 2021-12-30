be633bf69b8e2e268ea263da465eb0799eb80558
9sysjsy9 <9sysjsy9@naver.com>
2021-12-30 14:41:16 +0900

    [update] index3 추가

--- team01/src/main/java/com/kh/ex01/HomeController.java
+++ team01/src/main/java/com/kh/ex01/HomeController.java
@@ -25,5 +25,9 @@ public class HomeController {
    public String index2() {
       return "index2";
    }
+   @RequestMapping(value = "/index3", method = RequestMethod.GET)
+   public String index3() {
+      return "index2";
+   }
    
 }
