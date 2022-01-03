36b9fe67de751fb76790e693ac96b3442731e93c
0420sim <0420sim@naver.com>
2022-01-03 14:39:12 +0900

    [update] 채용공고 부분 수정및 업데이트

--- team01/src/main/java/com/kh/ex01/service/HireService.java
+++ team01/src/main/java/com/kh/ex01/service/HireService.java
@@ -1,5 +1,37 @@
 package com.kh.ex01.service;
 
-public class HireService {
+import java.util.List;
 
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+
+import com.kh.ex01.Dao.HireDao;
+import com.kh.ex01.vo.HireVo;
+
+@Service
+public class HireService {
+	
+	@Inject
+	HireDao hireDao;
+	
+	public List<HireVo> listAll() {
+		List<HireVo> list = hireDao.listAll();
+		return list;
+	}
+	
+	public List<HireVo> listNew() {
+		List<HireVo> list = hireDao.listNew();
+		return list;
+	}
+	
+	public List<HireVo> listExperience() {
+		List<HireVo> list = hireDao.listExperience();
+		return list;
+	}
+	
+	public List<HireVo> listIntern() {
+		List<HireVo> list = hireDao.listIntern();
+		return list;
+	}
 }
