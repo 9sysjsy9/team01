492a2531db55bbab7e8058a7e5445d8bdca2e79a
Lavienne <tarzanmon@naver.com>
2022-01-06 10:44:14 +0900

    [만재] 사내 하이어 항목 수정

---
+++ team01/src/main/java/com/kh/ex01/interceptor/AuthIntercemtor.java
@@ -0,0 +1,24 @@
+package com.kh.ex01.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import com.kh.ex01.vo.MemberVo;
+
+public class AuthIntercemtor extends HandlerInterceptorAdapter{
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		HttpSession session = request.getSession();
+		MemberVo memberVo = (MemberVo)session.getAttribute("memberVo");
+		// TODO Auto-generated method stub
+		return super.preHandle(request, response, handler);
+	}
+	
+	
+
+}
