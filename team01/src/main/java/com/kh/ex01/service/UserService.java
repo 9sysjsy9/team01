dc237f6e760a0154aeaec60a80ebec67e6fc4003
aseraph <unseraph@naver.com>
2022-01-11 12:50:38 +0900

    ㅁㄴㅇㅁㄴㅇ

--- team01/src/main/java/com/kh/ex01/service/UserService.java
+++ team01/src/main/java/com/kh/ex01/service/UserService.java
@@ -13,10 +13,10 @@ import com.kh.ex01.vo.UserVo;
 
 @Service
 public class UserService {
-	
+
 	@Inject
 	private UserDao userDao;
-	
+
 	public UserVo loginRun(UserVo userVo2) {
 		UserVo userVo = userDao.loginRun(userVo2);
 		return userVo;
